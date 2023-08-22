ea5df03e1450dcc958bf991ce3db53795a240c43
nicweh <131185062+nicweh@users.noreply.github.com>
2023-08-22 15:57:01 +0200

    Update color.dart

--- lib/color.dart
+++ lib/color.dart
@@ -5,4 +5,4 @@ const meinePrimLight = const Color(0xffe8eaf6);
 const meinePrimDunke = const Color(0xff1a237e);
 const meineSekuFarbe = const Color(0xff18ffff);
 const meineSekuLight = Color.fromARGB(255, 8, 109, 25);
-const meineSekuDunke = Color.fromARGB(255, 209, 233, 236);
+const meineSekuDunke = Color.fromARGB(255, 248, 248, 248);
